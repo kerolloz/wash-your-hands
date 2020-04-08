a45eec48dc59997e4ab19db4a9054663bda377e1
kerollos <kerolloz@yahoo.com>
2020-04-08 13:14:31 +0200

    change tweeting policy

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'twitter', require: false
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'whenever', require: false
--- main.rb
+++ main.rb
@@ -10,5 +10,21 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV["ACCESS_SECRET"]
 end
 
-client.update('This is my first bot tweet!')
-puts "Tweet sent"
+
+tweets = [
+  "Did you wash your hands today? 🤔",
+  "Go wash your hands! NOW!",
+  "Pssst, Wash your hands!",
+  "You know what? You should wash your hands!",
+  "Why don't you get up and Wash your hands!",
+  "Hey you! Wash your hands!",
+  "Time to wash your hands!",
+  "WASH YOUR HANDS!!!"
+]
+
+while true
+  to_be_tweeted = tweets.sample
+  client.update(to_be_tweeted)
+  puts to_be_tweeted
+  sleep(15 * 60) # 15 minutes => to seconds
+end
